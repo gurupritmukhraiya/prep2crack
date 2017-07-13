c55f5853243c237f9802cc021cc00001ad035143
Mukhraiya (EXT) <pd9rgf@net.plm.eds.com>
2017-07-13 15:18:17 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var Reviews = require('./public/src/js/dao/ReviewDAO');
+
+app.use(express.static('public'));
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH');
+    res.header('Access-Control-Allow-Headers', 'Content-type, Accept, X-Access-Token, X-Key');
+    next();
+});
+
+app.use('/review', Reviews);
+
+var server = app.listen(8080, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+})
+
+module.exports = app;
