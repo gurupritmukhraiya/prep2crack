e6cab21cc184eb36c87c1e064b03443ab15438ca
Mukhraiya (EXT) <pd9rgf@net.plm.eds.com>
2017-07-24 18:00:27 +0530

    Latest Changes

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var Reviews = require('./public/src/js/dao/ReviewDAO');
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
